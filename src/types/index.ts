61ce6ee09ab02fafa2a8ebadc4d8cee9c474f7d5
Yasir Gaji <gajiyasir@gmail.com>
2024-01-05 10:22:49 +0100

    created a motion utility file in the utils folder

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export type Motion = {
+  delay: any;
+  direction: string;
+  type: any;
+  duration: any;
+  staggerChildren: any;
+  delayChildren: any
+}
